f47119ccacdee218169dd6dcfe3ec2f73e905239
Henry <henrysandsgrant@gmail.com>
2024-07-29 18:17:39 +0100

    Adding project details

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,10 +1,65 @@
 const projectsList = [
-    { id: 1, title: 'Project One', description: 'Description for project one.', image: 'https://via.placeholder.com/800x400', details: 'Detailed information about Project One' },
-    { id: 2, title: 'Project Two', description: 'Description for project two.', image: 'https://via.placeholder.com/800x400', details: 'Detailed information about Project Two' },
-    { id: 3, title: 'Project Three', description: 'Description for project three.', image: 'https://via.placeholder.com/800x400', details: 'Detailed information about Project Three' },
-    { id: 4, title: 'Project Four', description: 'Description for project four.', image: 'https://via.placeholder.com/800x400', details: 'Detailed information about Project Four' },
-    { id: 5, title: 'Project Five', description: 'Description for project five.', image: 'https://via.placeholder.com/800x400', details: 'Detailed information about Project Five' },
-    { id: 6, title: 'Project Six', description: 'Description for project six.', image: 'https://via.placeholder.com/800x400', details: 'Detailed information about Project Six' }
+    { id: 1, title: 'Automatic Pet-Feeder',
+        description: 'Description for project ten.',
+        image: 'images/pet-feeder.jpg',
+        details: 'Detailed information about Project Ten',
+        recommended: false
+    },
+    { id: 2, title: 'Replicating the F1 start light sequence using a PIC16F88 with Assembly',
+        description: 'Description for project nine.',
+        image: 'images/f1-lights.jpg',
+        details: 'Detailed information about Project Nine',
+        recommended: false
+    },
+    { id: 3, title: 'Designing a guitar tuner using a PIC16F88 with Assembly',
+        description: 'Description for project eight.',
+        image: 'images/guitar-tuner.jpg',
+        details: 'Detailed information about Project Eight',
+        recommended: false 
+    },
+    { id: 4, title: 'Designing & building an autonomous line-following robot with computer vision',
+        description: 'Description for project seven.',
+        image: 'images/autonomous-vehicle.jpg',
+        details: 'Detailed information about Project Seven',
+        recommended: true
+    },
+    { id: 5, title: 'Design, construction and testing of a fully controlled Two Switch Forward Converter with a regulated output voltage',
+        description: 'Description for project six.',
+        image: 'images/converter-pcb.png',
+        details: 'Detailed information about Project Six',
+        recommended: false
+    },
+    { id: 6, title: 'E-Recycle',
+        description: 'A web app to make recycling easier',
+        image: 'images/e-recycle.png',
+        details: 'Detailed information about Project Five',
+        recommended: false
+    },
+    { id: 7, title: 'Doppler radar based speed detector',
+        description: 'Description for project four.',
+        image: 'images/doppler-radar.png',
+        details: 'Detailed information about Project Four',
+        recommended: false
+    },
+    { id: 8, title: "DC-DC Converter PCB",
+        description: "DCDC converter PCB for the University of Nottingham's solar racing car",
+        image: 'images/dcdc-pcb.png',
+        details: 'Detailed information about Project Three',
+        recommended: true
+    },
+    { id: 9, title: 'Strategic energy management system for the University of Nottingham solar racing car',
+        description: "An optimised prediction model for the University of Nottingham's solar car",
+        image: 'images/solar-car-diagram.png',
+        details: 'Detailed information about Project Two',
+        recommended: true
+    },
+    { id: 10, title: 'Mission Control Software framework for a CubeSat application',
+        description: 'A framework for CubeSat teams to use to track and communicate with their satellite',
+        image: 'images/cubesat-mission.png',
+        details: 'Detailed information about Project One',
+        recommended: true
+    }
 ];
+
 
 export default projectsList;
